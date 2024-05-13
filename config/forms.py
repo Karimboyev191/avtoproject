1fc2a068742a12264231b891a3ed921d6144e2e2
Karimboyev191 <xudoybergancoder@gmail.com>
2024-05-13 14:20:51 +0500

    commit

--- config/forms.py
+++ config/forms.py
@@ -5,13 +5,27 @@ from pgapp.models import *
 class ZakazForm(forms.ModelForm):
     class Meta:
         model = Zakaz
-        fields = ('mijoz_fio', 'avtosi', 'rangi', 'diagnostika')
+        fields = ['mijoz_fio', 'avtosi', 'rangi', 'diagnostika']
+        labels={
+            'mijoz_fio': 'ФИО клиента',
+            'avtosi':'Aвтомобиль',
+            'rangi':'Цвет',
+            'diagnostika':'Диагностика',
+
+        }
+
 
 
 class DiagnostikalarForm(forms.ModelForm):
     class Meta:
         model = Diagnostikalar
         fields = ('zakaz_id','usta_id', 'usta_haqqi','servis')
+        labels={
+            'zakaz_id':'Заказ ИД',
+            'usta_id':'Рабочий',
+            'usta_haqqi':'Деньги сотрудника',
+            'servis':'Служба',
+        }
 
 
 
